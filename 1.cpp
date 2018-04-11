cbea86af9127bec4dfadbe0f2f1e0df4667dcc56
wjdtjq0630 <wjdtjq0630@gmail.com>
2018-04-11 23:59:15 +0900

    first commit

---
+++ 1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+main(){
+	/*
+	***** 1 05
+	 **** 2 14
+	  *** 3 23
+	   ** 4 32
+	    * 5 41
+	*/
+	int i,j,k,l;
+	scanf("%d",&l);
+	for(i=1;i<=l;i++){
+		for(j=1;j<i;j++){
+			printf(" ");
+		}
+		for(k=1;k<=)
+		printf("\n");
+	}
+}
---
+++ 이름없음1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+main(){
+	int i,j,k;
+	for(i=1;i<=5;i++){
+		for(j=1;j<=i;j++){
+			printf("*");		
+		}	
+		printf("\n");
+	}
+}
+212
+131
+050
+131
+212
